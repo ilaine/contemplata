228745dcc0a95838ead8661d56ef0f03351b73f2
ilaine <wang.ilaine@gmail.com>
2019-06-06 10:45:48 +0200

    Clean Rule.elm
    Cleaning old code (Kuba) and correcting "flattening" -> "deepening"

--- annotool/src/Edit/Rule.elm
+++ annotool/src/Edit/Rule.elm
@@ -2,7 +2,6 @@ module Edit.Rule exposing
   ( Rule
   , theRule
   , apply
-  -- , Context
   )
 
 import Maybe
@@ -79,7 +78,6 @@ type Pattern
       -- ^ Tree predicate
     | Seq Pattern (Lazy Pattern)
       -- ^ Sequential ordering: first one pattern, then the other one
-    -- | Star Pattern
     | Or Pattern Pattern
 
 
@@ -88,13 +86,13 @@ seq : Pattern -> Pattern -> Pattern
 seq x y = Seq x <| Lazy.lazy <| always y
 
 
--- | One or more patterns.  Greedy.
+-- | One or more patterns. Greedy.
 plus : Pattern -> Pattern
 plus pat =
     Seq pat <| Lazy.lazy (\() -> Or (star pat) Epsilon)
 
 
--- | Zero or more patterns.  Greedy.
+-- | Zero or more patterns. Greedy.
 star : Pattern -> Pattern
 star pat = Or (plus pat) Epsilon
 
@@ -203,16 +201,14 @@ compile cxt freeID tree =
 ------------------------------------------------------------
 
 
--- | The list of flattening rules.
+-- | The list of deepening rules.
 allRules : List Rule
 allRules =
     [ compile
           { parent = \x ->
                 List.member x ["Ssub"]
           , left = seq
-              -- (star <| rootSat <| \label -> label /= "CS")
               (optional <| root "PUNC")
-              -- (root "CS")
               (rootSat <| \x -> List.member x ["CS", "MWC"])
           , middle = star any
           , right = Epsilon
@@ -241,166 +237,6 @@ allRules =
 -- | "The" rule is the list of all rules compiled into one.
 theRule : Rule
 theRule = merge allRules
-
-
-------------------------------------------------------------
--- Rules
-------------------------------------------------------------
-
-
--- -- | The list of flattening rules.
--- allRules : List Rule
--- allRules =
---   let
---     mkRule v p x = if p x then Just v else Nothing
---   in
---     [ mkRule "VP" (\x ->
---         List.member x.parent ["SENT", "VPinf", "Srel"] &&
---         x.left == "VN" &&
---         x.right == "VPinf")
---     , mkRule "VP" (\x ->
---         List.member x.parent ["SENT", "Ssub", "COORD", "Sint"] &&
---         x.left == "VN" &&
---         x.right == "NP")
---     , mkRule "VP" (\x ->
---         List.member x.parent ["VPinf", "Ssub"] &&
---         x.left == "VN" &&
---         x.right == "PP")
---     , mkRule "VP" (\x ->
---         List.member x.parent ["SENT"] &&
---         x.left == "VN" &&
---         x.right == "ADV")
---     ]
---
---
--- -- | Compile the list of rules into a single rule.
--- compile : List Rule -> Rule
--- compile rules ctx = case rules of
---   [] -> Nothing
---   (r :: rs) -> case r ctx of
---     Nothing -> compile rs ctx
---     Just v  -> Just v
---
---
--- -- | "The" rule is the list of all rules compiled into one.
--- theRule : Rule
--- theRule = compile allRules
---
---
--- ------------------------------------------------------------
--- -- Rule types
--- ------------------------------------------------------------
---
---
--- type alias Context =
---     { parent : String
---     , left : List String
---     , match : List String
---     , right : List String
---       -- ^ All three lists should be non-empty
---     }
---
---
--- -- | If the rule returns `Just`, a new node (with the resulting label) can be
--- -- added over `left` and `right`, and below the `parent`.
--- type alias Rule = Context -> Maybe String
---
---
--- ------------------------------------------------------------
--- -- Application
--- ------------------------------------------------------------
---
---
--- -- | Apply the rule everywhere when it can be applied in the given tree, and
--- -- return the set of the IDs of the new nodes.
--- apply : Rule -> R.Tree M.Node -> (R.Tree M.Node, S.Set C.NodeId)
--- apply rule tree0 =
---   let
---     go tree curId = case tree of
---       R.Node root [] ->
---         { result = R.Node root []
---         , nodeSet = S.empty
---         , curId = curId }
---       R.Node root (x :: []) ->
---         let r = go x curId
---         in  {r | result = R.Node root (r.result :: [])}
---       R.Node root (x :: y :: xs) ->
---         let
---           value = Lens.get M.nodeVal
---           rootVal = value << R.label
---           context =
---             { parent = value root
---             , left   = rootVal x
---             , right  = rootVal y }
---         in
---           case rule context of
---             Nothing -> joinFirst (go x) (go (R.Node root (y :: xs))) curId
---             Just newLabel ->
---               let
---                 newNode = M.Node
---                   { nodeId = curId
---                   , nodeVal = newLabel
---                   , nodeTyp = Nothing
---                   , nodeComment = "" }
---               in
---                 joinFirst
---                   (addRoot newNode (go x) (go y))
---                   (go (R.Node root xs))
---                   (curId + 1)
---   in
---     (\r -> (r.result, r.nodeSet))
---     <| go tree0
---     <| (\i -> i+1)
---     <| maxID tree0
---
---
--- -- | The result of computation, kind of. Normally, we would use a monad here...
--- type alias CompRes =
---   { result : R.Tree M.Node
---     -- ^ The resulting tree
---   , nodeSet : S.Set C.NodeId
---     -- ^ The resulting set of node IDs
---   , curId : C.NodeId
---     -- ^ The current maximum ID
---   }
---
---
--- -- | A computation, which takes the max ID and returns the computation result.
--- type alias Comp = C.NodeId -> CompRes
---
---
--- joinFirst
---      : Comp -- x
---     -> Comp -- r xs
---     -> Comp -- r (x : xs)
--- joinFirst f g curId =
---   let
---     x = f curId
---     y = g x.curId
---   in
---     case y.result of
---       R.Node root forest ->
---         { result = R.Node root (x.result :: forest)
---         , nodeSet = S.union x.nodeSet y.nodeSet
---         , curId = y.curId
---         }
---
---
--- addRoot
---      : M.Node -- x
---     -> Comp   -- y
---     -> Comp   -- z
---     -> Comp   -- x [y, z]
--- addRoot x f g curId =
---   let
---     y = f curId
---     z = g y.curId
---   in
---     { result = R.Node x [y.result, z.result]
---     , nodeSet = S.union y.nodeSet z.nodeSet
---              |> S.insert (Lens.get M.nodeId x)
---     , curId = z.curId
---     }
 
 
 ------------------------------------------------------------
